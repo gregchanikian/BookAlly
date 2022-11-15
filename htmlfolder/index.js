8f7ef6eab163da488ccb838e70e93a83cba8083a
gregchanikian <t8200177@aueb.gr>
2022-11-15 11:29:38 +0200

    hfxchj

--- htmlfolder/index.js
+++ htmlfolder/index.js
@@ -4,6 +4,7 @@ var button = document.getElementById("create_account");
 var loginformsubmit = document.getElementById("loginformsubmit");
 var loginformfield = document.getElementById("loginformfield");
 var checkbox = document.getElementById("checkbox")
+var checkbox_title = document.getElementById("checkbox_title")
 
 button.onclick = function(){
 
@@ -12,12 +13,14 @@ button.onclick = function(){
         loginformsubmit = "";
         mainHolder.className = "";
         checkbox.className = "";
+        checkbox_title.className = "";
         button.innerHTML = "Create Account";
         location.href = 'wishlist.html';
 
       } else{
         checkbox.className = "open";
         mainHolder.className = "open";
+        checkbox_title.className = "open";
         button.innerHTML = "Login";
         button.style.fontSize = "140%";
 
