bfbf2be88f064a9ca7129dc36c9dfb53b895ab22
gregchanikian <t8200177@aueb.gr>
2023-01-08 15:20:15 +0200

    Update Recommendation.java

--- javafolder/Recommendation.java
+++ javafolder/Recommendation.java
@@ -8,6 +8,20 @@ import java.util.List;
 public class Recommendation {
 
 
+    public void createRecommendation(User user) throws Exception {
+
+        
+    }
+
+
+
+    public Book randomRecommendation() throws Exception {
+
+
+
+        return null;
+
+    }
 
 
 
@@ -15,13 +29,36 @@ public class Recommendation {
 
 
 
+    public void addToWishlist(int id, String username) throws Exception {
+
+        String sql = " INSERT INTO wishlist "
+        + " (id, username) ";
+        Connection con = null;
+        DB db = new DB();
+		try {
+
+            con = db.getConnection();
+            PreparedStatement stmt = con.prepareStatement(sql);
+            stmt.setInt( 1, id );
+            stmt.setString( 2, username );
+
+            
+            stmt.close();
+            db.close();
+
+
+            
+        } catch (Exception e) {
+            throw new Exception(e.getMessage());
+        } finally {
+            try {
+                db.close();
+            } catch (Exception e) {
+                
+            }
+        }
+    }
 
 
 
-
-
-
-
-
-    
 }
